40b4c2fc5f57c44a2aa737993420f460dfb93ba3
Kevdog5000 <24kou@brentwoodmoschools.org>
2021-11-22 16:13:19 -0600

    replaced the original
    Co-Authored-By: TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -4,109 +4,140 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "DriverControl", group = "Linear Opmode")
-//@Disabled
+// @Disabled
 public class DriverControl extends LinearOpMode {
 
   private ElapsedTime runtime = new ElapsedTime();
 
-  private DcMotor leftFront = null;
-  private DcMotor leftRear = null;
-  private DcMotor rightFront = null;
-  private DcMotor rightRear = null;
+  private DcMotor LEFT_FRONT = null;
+  private DcMotor LEFT_REAR = null;
+  private DcMotor RIGHT_FRONT = null;
+  private DcMotor RIGHT_REAR = null;
 
-  private DcMotor conveyor1 = null;
-  private DcMotor conveyor2 = null;
-  private DcMotor extender = null;
-  private Servo claw = null;
+  private DcMotor CONVEYOR_1 = null;
+  private DcMotor CONVEYOR_2 = null;
+  private DcMotor EXTENDER = null;
 
-  private Servo spinner = null;
+  private Servo CLAW = null;
+
+  private Servo SPINNER = null;
 
   @Override
   public void runOpMode() {
+    // Constants
+    /**
+     * why is it all caps? it's a constant
+     */
+    double PRECISION = 2;
+
+    // Components
+    LEFT_FRONT = hardwareMap.get(DcMotor.class, "left_front");
+    LEFT_REAR = hardwareMap.get(DcMotor.class, "left_rear");
+    RIGHT_FRONT = hardwareMap.get(DcMotor.class, "right_front");
+    RIGHT_REAR = hardwareMap.get(DcMotor.class, "right_rear");
+
+    CONVEYOR_1 = hardwareMap.get(DcMotor.class, "conveyor_1");
+    CONVEYOR_2 = hardwareMap.get(DcMotor.class, "conveyor_2");
+    EXTENDER = hardwareMap.get(DcMotor.class, "extender");
+    CLAW = hardwareMap.get(Servo.class, "claw");
+
+    SPINNER = hardwareMap.get(Servo.class, "spinner");
+
+    // One time executions
+    LEFT_FRONT.setDirection(DcMotor.Direction.REVERSE);
+    RIGHT_REAR.setDirection(DcMotor.Direction.REVERSE);
+
     telemetry.addData("Status", "Initialized");
     telemetry.update();
-
-    leftFront = hardwareMap.get(DcMotor.class, "left_front");
-    leftRear = hardwareMap.get(DcMotor.class, "left_rear");
-    rightFront = hardwareMap.get(DcMotor.class, "right_front");
-    rightRear = hardwareMap.get(DcMotor.class, "right_rear");
-
-    conveyor1 = hardwareMap.get(DcMotor.class, "conveyor_1");
-    conveyor2 = hardwareMap.get(DcMotor.class, "conveyor_2");
-    extender = hardwareMap.get(DcMotor.class, "extender");
-    claw = hardwareMap.get(Servo.class, "claw");
-
-    spinner = hardwareMap.get(Servo.class, "spinner");
-
-    leftFront.setDirection(DcMotor.Direction.REVERSE);
-    rightRear.setDirection(DcMotor.Direction.REVERSE);
 
     waitForStart();
     runtime.reset();
 
     while (opModeIsActive()) {
+      // Constants
+
+      /**
+       * dividing by 1.5 because it's too fast at 100% power
+       *
+       * don't know what "a ? b : c" does? google ternary operators
+       * 😘 Abhi
+       */
+      double speedControl = gamepad1.left_bumper ? 2.5 : 1.5;
+
+      // dampen to not make it 1:1, it's an exponential growth
+      double dampedLeftJoystickX =
+        Math.signum(gamepad1.left_stick_x) *
+        Math.pow(gamepad1.left_stick_x, PRECISION);
+      double dampedLeftJoystickY =
+        Math.signum(gamepad1.left_stick_y) *
+        Math.pow(gamepad1.left_stick_y, PRECISION);
+      double dampedRightJoystickX =
+        Math.signum(gamepad1.right_stick_x) *
+        Math.pow(gamepad1.right_stick_x, PRECISION);
+      double dampedRightJoystickY =
+        Math.signum(gamepad1.right_stick_y) *
+        Math.pow(gamepad1.right_stick_y, PRECISION);
+
+      // resultant vectors (google it if yoy don't know)
       double vectorNormal = Math.hypot(
-        gamepad1.left_stick_y,
-        gamepad1.left_stick_x
+        dampedLeftJoystickY,
+        dampedLeftJoystickX
       );
       double robotAngle =
-        Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+        Math.atan2(dampedLeftJoystickY, -dampedLeftJoystickX) - Math.PI / 4;
+      // trig to find out partial offsets in axes (plural of axis)
       double v1 = vectorNormal * Math.cos(robotAngle);
       double v2 = vectorNormal * Math.sin(robotAngle);
       double v3 = vectorNormal * Math.sin(robotAngle);
       double v4 = vectorNormal * Math.cos(robotAngle);
 
-      double speedControl = gamepad1.left_bumper ? 2.5 : 1.5;
+      LEFT_FRONT.setPower((-v1 + dampedRightJoystickX) / speedControl);
+      LEFT_REAR.setPower((-v2 + dampedRightJoystickX) / speedControl);
+      RIGHT_FRONT.setPower((-v3 - dampedRightJoystickX) / speedControl);
+      RIGHT_REAR.setPower((-v4 - dampedRightJoystickX) / speedControl);
 
-      leftFront.setPower((-v1 + gamepad1.right_stick_x) / speedControl);
-      leftRear.setPower((-v2 + gamepad1.right_stick_x) / speedControl);
-      rightFront.setPower((-v3 - gamepad1.right_stick_x) / speedControl);
-      rightRear.setPower((-v4 - gamepad1.right_stick_x) / speedControl);
+      CONVEYOR_1.setPower(gamepad2.right_bumper ? 0.5 : 0);
+      CONVEYOR_2.setPower(gamepad2.right_bumper ? -0.5 : 0);
+      CONVEYOR_1.setPower(gamepad2.left_bumper ? -0.5 : 0);
+      CONVEYOR_2.setPower(gamepad2.left_bumper ? 0.5 : 0);
 
-      conveyor1.setPower(gamepad2.right_bumper ? 0.5 : 0);
-      conveyor2.setPower(gamepad2.right_bumper ? -0.5 : 0);
-      conveyor1.setPower(gamepad2.left_bumper ? -0.5 : 0);
-      conveyor2.setPower(gamepad2.left_bumper ? 0.5 : 0);
+      EXTENDER.setPower(gamepad2.right_trigger);
+      EXTENDER.setPower(-gamepad2.left_trigger);
 
-      extender.setPower(gamepad2.right_trigger);
-      extender.setPower(-gamepad2.left_trigger);
+      SPINNER.setPosition(gamepad1.right_bumper ? 1 : 0.49);
 
-      spinner.setPosition(gamepad1.right_bumper ? 1 : 0.49);
+      CLAW.setPosition(gamepad2.dpad_right ? 1 : 0);
 
-      claw.setPosition(gamepad2.dpad_right ? 1 : 0);
-
-      // conveyor1.setPower(gamepad2.right_stick_y);
-      // conveyor2.setPower(-gamepad2.right_stick_y);
-      // conveyor1.setPower(-gamepad2.right_stick_y);
-      // conveyor2.setPower(gamepad2.right_stick_y);
-
-      double drive = -gamepad1.left_stick_y;
-      double turn = gamepad1.right_stick_x;
+      double drive = -dampedLeftJoystickY;
+      double turn = dampedRightJoystickX;
 
       telemetry.addData("Status", "Run Time: " + runtime.toString());
 
-      telemetry.addData("LF Power", leftFront.getPower());
-      telemetry.addData("LR Power", leftRear.getPower());
-      telemetry.addData("RF Power", rightFront.getPower());
-      telemetry.addData("RR Power", rightRear.getPower());
+      telemetry.addData("LF Power", LEFT_FRONT.getPower());
+      telemetry.addData("LR Power", LEFT_REAR.getPower());
+      telemetry.addData("RF Power", RIGHT_FRONT.getPower());
+      telemetry.addData("RR Power", RIGHT_REAR.getPower());
 
       telemetry.addData("Left Joystick X", gamepad1.left_stick_x);
       telemetry.addData("Left Joystick Y", gamepad1.left_stick_y);
       telemetry.addData("Right Joystick X", gamepad1.right_stick_x);
       telemetry.addData("Right Joystick Y", gamepad1.right_stick_y);
 
+      telemetry.addData("Dampened Left Joystick X", dampedLeftJoystickX);
+      telemetry.addData("Dampened Left Joystick Y", dampedLeftJoystickY);
+      telemetry.addData("Dampened Right Joystick X", dampedRightJoystickX);
+      telemetry.addData("Dampened Right Joystick Y", dampedRightJoystickY);
+
       telemetry.addData("Right DPad", gamepad2.dpad_right);
 
-      telemetry.addData("Conveyor 1", conveyor1.getPower());
-      telemetry.addData("Conveyor 2", conveyor2.getPower());
+      telemetry.addData("Conveyor 1", CONVEYOR_1.getPower());
+      telemetry.addData("Conveyor 2", CONVEYOR_2.getPower());
 
-      telemetry.addData("Claw Value", claw.getPosition());
+      telemetry.addData("Claw Value", CLAW.getPosition());
 
       telemetry.update();
     }
