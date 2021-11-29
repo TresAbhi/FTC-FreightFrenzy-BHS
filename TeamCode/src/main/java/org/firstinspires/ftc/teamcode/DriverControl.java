4bce49dd483f0d08940eb39bb9ab2a429470b5b2
Kevdog5000 <24kou@brentwoodmoschools.org>
2021-11-29 16:36:06 -0600

    tweak arm
    Co-Authored-By: TrèsAbhi <43380238+TresAbhi@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -36,14 +36,21 @@ public class DriverControl extends LinearOpMode {
     // Constants
     double MOVEMENT_PRECISION = 2;
 
-    double ARM_CATCH_UP_MAX_POWER = 0.5; // when the arm is at ARM_CATCHUP_ACCPECTANCE_RANGE diffrence, it will approach this power value [-1.0, +1]
-    double ARM_CATCHUP_ACCPECTANCE_RANGE = 25;
+    double ARM_CATCH_UP_MAX_POWER = 0.5; // when the arm is at ARM_CATCHUP_ACCEPTANCE_RANGE diffrence, it will approach this power value [-1.0, +1]
+    double ARM_CATCHUP_ACCEPTANCE_RANGE = 20;
     int ARM_CATCH_UP_INPUT_SPEED = 3;
-    int ARM_POS_MIN = -422;
+    int ARM_POS_MIN = 10;
     int ARM_POS_MAX = ARM_POS_MIN + 405;
+
+    double EXTENDER_CATCH_UP_MAX_POWER = 0.5;
+    double EXTENDER_CATCHUP_ACCEPTANCE_RANGE = 20;
+    float EXTENDER_CATCH_UP_INPUT_SPEED = 15;
+    float EXTENDER_POS_MIN = -10;
+    float EXTENDER_POS_MAX = EXTENDER_POS_MIN + 1575;
 
     // Mutables
     int armTargetPosition = ARM_POS_MIN;
+    float extenderTargetPosition = EXTENDER_POS_MIN;
 
     String driveMode = "normal";
     boolean isModeSwitched = false;
@@ -101,17 +108,26 @@ public class DriverControl extends LinearOpMode {
        */
       double speedControl = g1.left_bumper ? 2.5 : 1.5;
 
-      double currentPosition = ARM_JOINT_LEFT.getCurrentPosition();
-      double armPosDiffRaw = armTargetPosition - currentPosition;
-      double armPosDiffPartial = armPosDiffRaw / ARM_CATCHUP_ACCPECTANCE_RANGE;
+      double armCurrentPosition = ARM_JOINT_LEFT.getCurrentPosition();
+      double armPosDiffRaw = armTargetPosition - armCurrentPosition;
+      double armPosDiffPartial = armPosDiffRaw / ARM_CATCHUP_ACCEPTANCE_RANGE;
       double armPosDiffCoefficient =
         Math.signum(armPosDiffPartial) *
         Math.min(Math.abs(armPosDiffPartial), 1);
+
+      double extenderCurrentPosition = EXTENDER.getCurrentPosition();
+      double extenderPosDiffRaw =
+        extenderTargetPosition - extenderCurrentPosition;
+      double extenderPosDiffPartial =
+        extenderPosDiffRaw / EXTENDER_CATCHUP_ACCEPTANCE_RANGE;
+      double extenderPosDiffCoefficient =
+        Math.signum(extenderPosDiffPartial) *
+        Math.min(Math.abs(extenderPosDiffPartial), 1);
+
       // dampen to not make it 1:1, it's an exponential growth
       double dampedLeftJoystickX =
         Math.signum(g1.left_stick_x) *
         Math.pow(g1.left_stick_x, MOVEMENT_PRECISION);
-
       double dampedLeftJoystickY =
         Math.signum(g1.left_stick_y) *
         Math.pow(g1.left_stick_y, MOVEMENT_PRECISION);
@@ -139,17 +155,35 @@ public class DriverControl extends LinearOpMode {
       LEFT_REAR.setPower((-v2 + dampedRightJoystickX) / speedControl);
       RIGHT_FRONT.setPower((-v3 - dampedRightJoystickX) / speedControl);
       RIGHT_REAR.setPower((-v4 - dampedRightJoystickX) / speedControl);
+
       if (g2.right_bumper) armTargetPosition =
         Math.min(armTargetPosition + ARM_CATCH_UP_INPUT_SPEED, ARM_POS_MAX);
-
       if (g2.left_bumper) armTargetPosition =
         Math.max(armTargetPosition - ARM_CATCH_UP_INPUT_SPEED, ARM_POS_MIN);
+
       ARM_JOINT_LEFT.setPower(armPosDiffCoefficient * ARM_CATCH_UP_MAX_POWER);
-
       ARM_JOINT_RIGHT.setPower(armPosDiffCoefficient * ARM_CATCH_UP_MAX_POWER);
-      EXTENDER.setPower(g2.right_trigger);
 
-      EXTENDER.setPower(-g2.left_trigger);
+      extenderTargetPosition =
+        Math.min(
+          extenderTargetPosition +
+          (EXTENDER_CATCH_UP_INPUT_SPEED * g2.right_trigger),
+          EXTENDER_POS_MAX
+        );
+      extenderTargetPosition =
+        Math.max(
+          extenderTargetPosition -
+          (EXTENDER_CATCH_UP_INPUT_SPEED * g2.left_trigger),
+          EXTENDER_POS_MIN
+        );
+
+      EXTENDER.setPower(
+        extenderPosDiffCoefficient * EXTENDER_CATCH_UP_MAX_POWER
+      );
+
+      // EXTENDER.setPower(g2.right_trigger);
+      // EXTENDER.setPower(-g2.left_trigger);
+
       SPINNER.setPosition(g1.right_bumper ? 1 : 0.49);
 
       CLAW.setPosition(g2.dpad_right ? 1 : 0);
@@ -162,6 +196,12 @@ public class DriverControl extends LinearOpMode {
 
       telemetry.addData("Status", "Run Time: " + runtime.toString());
       telemetry.addData("Drive mode", driveMode);
+
+      telemetry.addData("Extender position", EXTENDER.getCurrentPosition());
+      telemetry.addData("Extender target position", extenderTargetPosition);
+
+      telemetry.addData("Arm position", ARM_JOINT_LEFT.getCurrentPosition());
+      telemetry.addData("Arm target position", armTargetPosition);
 
       telemetry.update();
     }
