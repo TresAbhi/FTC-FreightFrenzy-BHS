9c3e2c77a330094724e5fbb3c95cdb85aa645e3f
Kevdog5000 <24kou@brentwoodmoschools.org>
2022-02-10 16:46:10 -0600

    added clmainable god mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -35,9 +35,10 @@ public class DriverControl extends LinearOpMode {
     GOD
   }
 
+  public int godPlayer;
+
   // mutables
   DRIVE_MODE driveMode = DRIVE_MODE.NORMAL;
-  boolean isModeSwitched = false;
 
   // @Override
   public void runOpMode() {
@@ -51,24 +52,37 @@ public class DriverControl extends LinearOpMode {
     runtime.reset();
 
     while (opModeIsActive()) {
-      // Switch between modes
-      if (driveMode == DRIVE_MODE.NORMAL) {
-        player1 = gamepad1;
-        player2 = gamepad2;
-      } else if (driveMode == DRIVE_MODE.GOD) {
-        player1 = gamepad1;
-        player2 = gamepad1;
-      }
-
       // Power modes for slow and fast robot speeds
       drive.movementPower = 0.75f - (player1.left_trigger * 0.5f);
 
       // God mode toggler
-      if ((player1.back || player2.back) && !isModeSwitched) {
-        driveMode = driveMode == DRIVE_MODE.NORMAL ? DRIVE_MODE.GOD : DRIVE_MODE.NORMAL;
-        isModeSwitched = true;
+      if (gamepad1.back) {
+        if (driveMode == DRIVE_MODE.NORMAL) {
+          player1 = gamepad1;
+          player2 = gamepad1;
+          driveMode = DRIVE_MODE.GOD;
+          godPlayer = 1;
+        } else {
+          player1 = gamepad1;
+          player2 = gamepad2;
+          driveMode = DRIVE_MODE.NORMAL;
+          godPlayer = 0;
+        }
+        while (gamepad1.back) sleep(1);
+      } else if (gamepad2.back) {
+        if (driveMode == DRIVE_MODE.NORMAL) {
+          player1 = gamepad2;
+          player2 = gamepad2;
+          driveMode = DRIVE_MODE.GOD;
+          godPlayer = 2;
+        } else {
+          player1 = gamepad1;
+          player2 = gamepad2;
+          driveMode = DRIVE_MODE.NORMAL;
+          godPlayer = 0;
+        }
+        while (gamepad2.back) sleep(1);
       }
-      if (!(player1.back || player2.back)) isModeSwitched = false;
 
       // Tweak spinner joint speed
       drive.spinnerJointPos = player1.right_trigger;
@@ -139,7 +153,8 @@ public class DriverControl extends LinearOpMode {
       // Update telemetry
       telemetry.addData("Status", "Run Time: " + runtime.toString());
       telemetry.addData("Drive mode", driveMode);
-      telemetry.addData("Battery Voltage", drive.getBatteryVoltage());
+      telemetry.addData("Battery voltage", drive.getBatteryVoltage());
+      telemetry.addData("God player", godPlayer == 0 ? "none" : godPlayer);
       telemetry.addData("----------", "----------");
 
       drive.logTargetsAndCurrents();
